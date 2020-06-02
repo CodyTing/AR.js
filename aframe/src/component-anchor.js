c32027a01722dba4b3be6391cf0c2b047c2d5c5e
CodyTing <cting649@gmail.com>
2020-06-02 15:33:58 -0700

    ok

--- aframe/src/component-anchor.js
+++ aframe/src/component-anchor.js
@@ -113,6 +113,11 @@ AFRAME.registerComponent('arjs-anchor', {
                 markerParameters.type = _this.data.type
                 markerParameters.patternUrl = '../myApp/marker.patt';
                 markerParameters.markersAreaEnabled = false
+                }
+            else if (_this.data.preset === 'meep') {
+                    markerParameters.type = 'pattern'
+                    markerParameters.patternUrl = THREEx.ArToolkitContext.baseURL + 'examples/marker-training/examples/pattern-files/pattern-hiro.patt'
+                    markerParameters.markersAreaEnabled = false
             }
 
             markerParameters.smooth = _this.data.smooth;
